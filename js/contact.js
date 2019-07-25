a186612f3b7365cbe36e264d2bdb69104f1db0bd
Fiyinfoluwa6 <oyelamifiyin@gmail.com>
2019-07-25 13:15:12 +0100

    first commit

---
+++ js/contact.js
@@ -0,0 +1,39 @@
+﻿//console.log('it works')
+
+//$(document).ready(function(){
+//    $('.main-btn').click(function (event){
+//        event.preventDefault()
+//        console.log('Clicked button')
+        
+//        var name = $('.input').val()
+//        var email = $('.input').val()
+//        var subject = $('.input').val()
+//        var message = $('.input').val()
+//        statusElm.empty()
+
+//        if (name.length >= 5) {
+//            statusElm.append('<div>Name is valid</div>')
+//        } else {
+//            statusElm.append('<div>Name is not valid</div>')
+//        }
+
+//        if (email.length > 15 && email.includes('@') && email.includes('.')) {
+//            statusElm.append('<div>Email is valid</div>')
+//        } else {
+//            statusElm.append('<div>Email is not valid</div>')
+//        }
+
+//        if (subject.length >=15 ) {
+//            statusElm.append('<div>subject is valid</div>')
+//        } else {
+//            statusElm.append('<div>subject is not valid</div>')
+//        }
+//        if (message.length >= 15) {
+//            statusElm.append('<div>message is valid</div>')
+//        } else {
+//            statusElm.append('<div>message is not valid</div>')
+//        }
+//    })
+
+
+//})
